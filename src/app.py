24b199c6ad6ff5c9209e11dbd44822ef5558c51d
Aki Pehkonen <aki.pehkonen@nortal.com>
2020-12-07 18:16:38 +0200

    try to get more accurate temperature

--- src/app.py
+++ src/app.py
@@ -5,8 +5,6 @@ from sense_hat import SenseHat
 from time import sleep
 from google.cloud import pubsub_v1
 
-sense = SenseHat()
-
 project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
 topic_id = os.getenv('GOOGLE_CLOUD_TOPIC_ID')
 device_id = os.getenv('DEVICE_ID')
@@ -14,14 +12,27 @@ device_id = os.getenv('DEVICE_ID')
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_id)
 
+sense = SenseHat()
+
 while True:
-    temperature = sense.get_temperature()
+    temp = sense.get_temperature()
+    t = os.popen('/opt/vc/bin/vcgencmd measure_temp')
+    cputemp = t.read()
+    cputemp = cputemp.replace('temp=','')
+    cputemp = cputemp.replace('\'C\n','')
+    cputemp = float(cputemp)
+    temperature = temp - ((cputemp - temp) / 2)
 
     event = {
         'measured_at': datetime.utcnow().isoformat(),
         'device': device_id,
-        'temperature': temperature
+        'temperature': temperature,
     }
 
-    future = publisher.publish(topic_path, json.dumps(event, default=str).encode('utf-8'))
-    sleep(7200)
+    payload = json.dumps(event, default=str).encode('utf-8')
+
+    print(payload)
+
+    future = publisher.publish(topic_path, payload)
+
+    sleep(15)
